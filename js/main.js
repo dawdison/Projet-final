89e11b95d7241cb7c7e61f720a5de891b42111df
dawdison <dawdison99@gmail.com>
2025-02-27 14:18:26 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function formbtn() {
+    var x = document.getElementById("formulaire");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
